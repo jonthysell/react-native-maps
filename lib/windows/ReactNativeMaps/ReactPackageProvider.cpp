884dc7cfbef90a15bd8a5a8400268027b7db976e
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2020-04-01 14:11:49 -0700

    Added empty package

---
+++ lib/windows/ReactNativeMaps/ReactPackageProvider.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "ReactPackageProvider.h"
+#include "ReactPackageProvider.g.cpp"
+
+#include <ModuleRegistration.h>
+
+// NOTE: You must include the headers of your native modules here in
+// order for the AddAttributedModules call below to find them.
+
+namespace winrt::ReactNativeMaps::implementation
+{
+    void ReactPackageProvider::CreatePackage(IReactPackageBuilder const& packageBuilder) noexcept
+    {
+        AddAttributedModules(packageBuilder);
+    }
+}
